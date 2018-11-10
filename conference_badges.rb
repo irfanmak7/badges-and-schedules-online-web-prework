8cbe88e6dc652e79b1a2b4c84575ce45aa7037dd
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-10 03:23:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(array)
   return new_array
 end
 
-def printer
+def printer(array)
   batch_badge_creator(array).each do |id|
     puts id
   end
@@ -29,7 +29,3 @@ def printer
     puts id
   end
 end
-
-
-
-  
